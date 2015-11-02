a919771d03d21cec27b0d0a83829e62bab3e2202
Mandy Choi <hello@mandychoi.com.au>
2015-11-02 00:37:47 +0000

    Init commit

---
+++ android/src/com/codemonkeys/spike/libgdx/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.codemonkeys.spike.libgdx.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.codemonkeys.spike.libgdx.LibGdxSpike;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LibGdxSpike(), config);
+	}
+}
