3e748284ab48b523940d9388dfb82dafb1d3f54e
Mandy Choi <hello@mandychoi.com.au>
2015-11-02 22:04:16 +0000

    #3 drops LOL face on touch

--- core/src/com/codemonkeys/spike/libgdx/LibGdxSpike.java
+++ core/src/com/codemonkeys/spike/libgdx/LibGdxSpike.java
@@ -9,19 +9,43 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class LibGdxSpike extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+    int currentXpos;
+    int currentYpos;
+
+    int speed = 5;
+
+    boolean hasDropped = false;
+    boolean isDropping = false;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+        // set up graphics
+        batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+        // set up game entities
+        currentXpos = Gdx.graphics.getWidth() / 2 - img.getWidth() / 2;
+        currentYpos = Gdx.graphics.getHeight() - img.getHeight();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+        // set to white
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, currentXpos, currentYpos);
 		batch.end();
+
+        if (!hasDropped && Gdx.input.isTouched()) {
+            isDropping = true;
+        }
+
+        if (isDropping) {
+            if (currentYpos > 0) {
+                currentYpos -= speed * Gdx.graphics.getDeltaTime();
+            }
+        }
 	}
 }
