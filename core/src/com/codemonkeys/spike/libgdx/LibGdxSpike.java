2af549a0579d644a06bf357b46dd20e395fa3b18
Mandy Choi <hello@mandychoi.com.au>
2015-11-02 22:18:20 +0000

    Revert "#3 drops LOL face on touch"
    This reverts commit 3e748284ab48b523940d9388dfb82dafb1d3f54e.

--- core/src/com/codemonkeys/spike/libgdx/LibGdxSpike.java
+++ core/src/com/codemonkeys/spike/libgdx/LibGdxSpike.java
@@ -9,43 +9,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class LibGdxSpike extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-
-    int currentXpos;
-    int currentYpos;
-
-    int speed = 5;
-
-    boolean hasDropped = false;
-    boolean isDropping = false;
 	
 	@Override
 	public void create () {
-        // set up graphics
-        batch = new SpriteBatch();
+		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-
-        // set up game entities
-        currentXpos = Gdx.graphics.getWidth() / 2 - img.getWidth() / 2;
-        currentYpos = Gdx.graphics.getHeight() - img.getHeight();
 	}
 
 	@Override
 	public void render () {
-        // set to white
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, currentXpos, currentYpos);
+		batch.draw(img, 0, 0);
 		batch.end();
-
-        if (!hasDropped && Gdx.input.isTouched()) {
-            isDropping = true;
-        }
-
-        if (isDropping) {
-            if (currentYpos > 0) {
-                currentYpos -= speed * Gdx.graphics.getDeltaTime();
-            }
-        }
 	}
 }
