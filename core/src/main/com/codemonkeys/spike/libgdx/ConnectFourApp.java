6c5c3a5ac353e8c19463e019f3f460fbbd7b627b
Mandy Choi <hello@mandychoi.com.au>
2015-12-15 17:13:11 +0000

    #8 camera position 0, 0 to represent bottom-left of the board

--- core/src/main/com/codemonkeys/spike/libgdx/ConnectFourApp.java
+++ core/src/main/com/codemonkeys/spike/libgdx/ConnectFourApp.java
@@ -12,13 +12,12 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 public class ConnectFourApp extends ApplicationAdapter {
-    // TODO-MC make this configurable based on board size
+    // Game settings
     private static final int NUM_OF_COLUMNS = 7;
     private static final int NUM_OF_ROWS = 6;
 
-    // TODO make this configurable
+    // Display settings
     private static final int PADDING = 1;
-
     private static final int APP_WIDTH = NUM_OF_COLUMNS + 2 * PADDING; // 7 columns with 1 PADDING on both side
     private static final int APP_HEIGHT = NUM_OF_ROWS + 2 * PADDING; // 6 rows with 1 PADDING above and below
 
@@ -28,23 +27,22 @@ public class ConnectFourApp extends ApplicationAdapter {
     private Sprite redSprite;
     private Sprite yellowSprite;
     private TextureRegion emptySlotTxRegion;
+    private ExtendViewport viewport;
 
     // this is only used locally to transform the input position,
     // but to prevent GC, just create it once here
     private final Vector3 touchPosition = new Vector3();
-    private ExtendViewport viewport;
 
     @Override
 	public void create () {
-        // Refer to: https://github.com/libgdx/libgdx/wiki/Orthographic-camera
         sprite = new Texture("c4_sprite.png");
 
         redSprite = new Sprite(sprite, 0, 0, 64, 64);
-        redSprite.setPosition(1, 7);
+        redSprite.setPosition(0, 6);
         redSprite.setSize(1, 1);
 
         yellowSprite = new Sprite(sprite, 64, 0, 64, 64);
-        yellowSprite.setPosition(7, 7);
+        yellowSprite.setPosition(6, 6);
         yellowSprite.setSize(1, 1);
 
         emptySlotTxRegion = new TextureRegion(sprite, 128, 0, 64, 64);
@@ -53,7 +51,7 @@ public class ConnectFourApp extends ApplicationAdapter {
         viewport = new ExtendViewport(APP_WIDTH, APP_HEIGHT, camera);
         viewport.apply();
 
-        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+        camera.position.set(camera.viewportWidth / 2f - PADDING, camera.viewportHeight / 2f - PADDING, 0);
         camera.update();
 
         batch = new SpriteBatch();
@@ -73,7 +71,7 @@ public class ConnectFourApp extends ApplicationAdapter {
 
         for (int y = 0; y < NUM_OF_ROWS; y++) {
             for (int x = 0; x < NUM_OF_COLUMNS; x++) {
-                batch.draw(emptySlotTxRegion, x + PADDING, y + PADDING, 1, 1);
+                batch.draw(emptySlotTxRegion, x, y, 1, 1);
             }
         }
 
@@ -85,8 +83,7 @@ public class ConnectFourApp extends ApplicationAdapter {
     @Override public void resize (int width, int height) {
         viewport.update(width, height);
 
-        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
-
+        camera.position.set(camera.viewportWidth / 2f - PADDING, camera.viewportHeight / 2f - PADDING, 0);
     }
 
     @Override public void dispose () {
